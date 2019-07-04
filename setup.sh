8bf402228b049f16273104b45aca6e302319d4f2
ocular-d <sven@ocular-d.tech>
2019-07-04 11:33:32 +0200

    refactor(vscode): gett settings from gist

--- setup.sh
+++ setup.sh
@@ -42,7 +42,13 @@ npm_modules() {
 update_bashrc() {
     echo 'export PATH="$PATH:/home/vagrant/node_modules/.bin"' >> ~/.bashrc
 }
+
+update_vscode_settings() {
+    wget https://gist.githubusercontent.com/ocular-d/cda72372a8168f0711700d417fa8a13e/raw/5f23d12b395dd62d271c02e5ee3af26a000c06c7/settings.json -O /$HOME/.config/Code/User/settings.json
+}
+
 # Run functions
 extensions
 npm_modules
 update_bashrc
+update_vscode_settings
